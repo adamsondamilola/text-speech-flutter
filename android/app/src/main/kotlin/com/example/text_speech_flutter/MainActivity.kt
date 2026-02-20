da98e8e8281ebc8e35ec1072d98f211ac133fae0
adamsondamilola <adamsondamilola@gmail.com>
2026-02-20 17:02:24 +0100

    Updated

---
+++ android/app/src/main/kotlin/com/example/text_speech_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.text_speech_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import ffmpeg_kit_flutter_min_gpl
+import flutter_tts
+import share_plus
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FFmpegKitFlutterPlugin.register(with: registry.registrar(forPlugin: "FFmpegKitFlutterPlugin"))
+  FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
+}
